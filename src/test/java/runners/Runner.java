4c4f54e381c296669f1b8f1ee7a65ab4fd671a7b
omerfarukyeler <omerfarukyeler.ae@gmail.com>
2023-04-14 02:09:35 +0200

    firstcommit

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,24 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin ={
+                "pretty", "html:target/cucumber", "json:target/cucumber.json",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber-reports",
+                "rerun:target/failed.txt",
+                "timeline:test-output-thread/",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        features = "src/test/resources/UI_Feature",
+        glue = "stepdefinitions",
+        tags = "@US019", // Keyword Driven Testing
+        dryRun = false
+)
+
+public class Runner {}
