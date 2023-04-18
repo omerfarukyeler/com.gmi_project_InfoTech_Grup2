131d113288aca1948dc7eec9d1950858571598a9
omerfarukyeler <omerfarukyeler.ae@gmail.com>
2023-04-19 00:03:05 +0200

    firstcommit

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"ebfef146-e473-40b4-8633-00a3f531c963","feature":"US001 sign in butonu","scenario":"kullanici sign in butonuna tiklamali","start":1681434111186,"group":1,"content":"","tags":"@us001,","end":1681434120360,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"cda64dfd-8dc4-42f1-b205-4ba5354fb7a8","feature":"US001 sign in butonu","scenario":"kullanici sign in butonuna tiklamali","start":1681855355094,"group":1,"content":"","tags":"@us001,","end":1681855376331,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
